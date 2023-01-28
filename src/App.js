9d9197a0e84b8f66034968acc970ee1330f43578
mnmandahalf <manaminakamura812@gmail.com>
2023-01-28 17:24:22 +0900

    add monaco-editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import MonacoEditor from 'react-monaco-editor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-main">
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="javascript"
+          theme="vs-dark"
+          value={""}
+          options={{}}
+          onChange={()=>{}}
+          editorDidMount={()=>{}}
+        />
+      </div>
     </div>
   );
 }
